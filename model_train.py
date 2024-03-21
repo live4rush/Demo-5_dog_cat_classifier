e1e70e6357056f9e09da465301e7b18e61fd244e
Naveen Neppalli <nnaveen356@gmail.com>
2024-03-20 19:18:17 -0700

    correct model_train.py

--- model_train.py
+++ model_train.py
@@ -2,7 +2,6 @@ import tensorflow as tf
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
-import joblib
 
 # Initialising the CNN
 classifier = Sequential()
